b744c9fb50b1a8535e0d7e0d912c8fb89602c3c9
h21anlus <h21anlus@du.se>
2022-02-24 14:11:32 +0100

    fö13 forts

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import cv2 # ska laddas ner
 image = cv2.imread('index.png')
 # converting image to rgb
 # cv2.cvtColor(src, code[, dst[, dstCn]]) -> dst
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # BGR till RGB, converts an image from one color space to another
 #plotting the image
 plt.imshow(image)
 plt.show()
