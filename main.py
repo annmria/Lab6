046090135afacd4b9281b2fa73ea286289c99b83
h21anlus <h21anlus@du.se>
2022-02-24 14:09:52 +0100

    copy från föreläsning 13

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2 # ska laddas ner
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+#reading the image:
+image = cv2.imread('index.png')
+# converting image to rgb
+# cv2.cvtColor(src, code[, dst[, dstCn]]) -> dst
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#plotting the image
+plt.imshow(image)
+plt.show()
+#saving image
+cv2.imwrite('index.jpg', image)
